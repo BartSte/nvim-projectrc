07fc285312cc4236da38f5022057b63ad883b4a1
barts <bartsteensma@outlook.com>
2024-03-12 20:17:32 +0100

    fix: check opts does not account for nil values

--- lua/projectrc/helpers.lua
+++ lua/projectrc/helpers.lua
@@ -14,18 +14,6 @@ M.module_join = function(parent, ...)
 end
 
 
---- Check if the `opts` table has keys that are not in the `defaults` table. If it
---- does, raise an error.
----@param opts table The options to check.
----@param defaults table The table to check the options against.
-M.check_keys = function(opts, defaults)
-  for key, _ in pairs(opts) do
-    if not defaults[key] then
-      error("invalid option: " .. key)
-    end
-  end
-end
-
 --- Raise an error if the `opts` table: has keys that are not in the defaults
 --- `opts` table, if it is not a table. If it does not raise an error.
 ---@param opts table The options to check.
@@ -33,7 +21,6 @@ M.check_opts = function(opts, defaults)
   if type(opts) ~= "table" then
     error("opts must be a table")
   end
-  M.check_keys(opts, defaults)
 end
 
 --- Merge the `opts` table with the defaults `opts` table. If the `opts` table has
